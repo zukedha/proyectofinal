9d300065bdea8c64682a605c6eda03c0d1301923
zukedha <zukedha@gmail.com>
2023-01-13 20:16:55 -0600

    subiendo

---
+++ src/Triangle.ts
@@ -0,0 +1,10 @@
+import { Point2D } from './point2D.js';
+
+export class Triangle{
+  a: Point2D;
+  b: Point2D;
+  c: Point2D;
+   Triangle(a:Point2D , b:Point2D , c:Point2D )
+   { this.a = a; this.b = b; this.c = c;
+   }
+}
